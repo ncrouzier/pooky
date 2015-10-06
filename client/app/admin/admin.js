2b582011a9435f570ced50a94a05f68c4f6e96de
Nicolas Crouzier <nicolas.crouzier@nist.gov>
2015-10-06 16:58:37 -0400

    added passport auth

---
+++ client/app/admin/admin.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('pookyApp')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('admin', {
+        url: '/admin',
+        templateUrl: 'app/admin/admin.html',
+        controller: 'AdminCtrl'
+      });
+  });
